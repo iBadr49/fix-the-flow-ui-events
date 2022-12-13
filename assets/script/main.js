eb25a4d2483fd02c6477409a9a2c992d2df762fb
B28A212 <112857932+B28A212@users.noreply.github.com>
2022-12-13 17:28:48 +0100

    Animatie 1&2

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,9 @@
+
+
+
+
+/*----12---- Interactie Animatie*/
+
 let interaction = document.querySelector('a:nth-of-type(12)')
 
 interaction.addEventListener('click', jumpHandler)
@@ -5,4 +11,26 @@ interaction.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
+}
+
+/*----1---- Frontend Animatie*/
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', colorAnimation)
+frontend.addEventListener('animationend', colorAnimation)
+
+function colorAnimation() {
+  frontend.classList.toggle('colorAni')
+}
+
+/*----2---- Design Animatie*/
+
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('click', designRotate)
+design.addEventListener('animationend', designRotate)
+
+function designRotate() {
+  design.classList.toggle('rotate')
 }
